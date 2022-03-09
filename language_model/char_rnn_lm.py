652511603ae3a62dab6c7af08c53746dfc5d82be
Hoang Nguyen <hoang24@pdx.edu>
2022-03-08 22:02:47 -0800

    fix bugs in language model

--- language_model/char_rnn_lm.py
+++ language_model/char_rnn_lm.py
@@ -24,136 +24,136 @@ import os
 
 import tensorflow as tf
 from six.moves import cPickle as pickle
-# from tensorflow.keras.layers import RNN as rnn
+# from tensorflow.keras.layers import RNN
 from tensorflow_addons import seq2seq as legacy_seq2seq
 
 class CharRnnLm():
-  def __init__(self,
-               args,
-               training=False,
-               input_data=None,
-               targets=None,
-               initial_state=None):
-    self.args = args
-    if not training:
-      # args.batch_size = 10
-      args.seq_length = 1
+    def __init__(self,
+                 args,
+                 training=False,
+                 input_data=None,
+                 targets=None,
+                 initial_state=None):
+        self.args = args
+        if not training:
+            # args.batch_size = 10
+            args.seq_length = 1
 
-    cells = []
-    for _ in range(args.num_layers):
+        cells = []
+        for _ in range(args.num_layers):
+            from distutils.version import StrictVersion
 
-      from distutils.version import StrictVersion
-      if StrictVersion(tf.__version__) >= StrictVersion('1.5'):
-        cell = tf.compat.v1.nn.rnn_cell.LSTMCell(args.rnn_size, name='basic_lstm_cell')
-      else:
-        cell = tf.compat.v1.nn.rnn_cell.BasicLSTMCell(args.rnn_size)
-      if training and (args.output_keep_prob < 1.0 or args.input_keep_prob < 1.0):
-        cell = tf.compat.v1.nn.rnn_cell.DropoutWrapper(cell,
-                                  input_keep_prob=args.input_keep_prob,
-                                  output_keep_prob=args.output_keep_prob)
-      cells.append(cell)
+            if StrictVersion(tf.__version__) >= StrictVersion('1.5'):
+                cell = tf.compat.v1.nn.rnn_cell.LSTMCell(args.rnn_size, name='basic_lstm_cell')
+            else:
+                cell = tf.compat.v1.nn.rnn_cell.BasicLSTMCell(args.rnn_size)
+            if training and (args.output_keep_prob < 1.0 or args.input_keep_prob < 1.0):
+                cell = tf.compat.v1.nn.rnn_cell.DropoutWrapper(cell,
+                                                               input_keep_prob=args.input_keep_prob,
+                                                               output_keep_prob=args.output_keep_prob)
+            cells.append(cell)
 
-    self.cell = cell = tf.compat.v1.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=True)
+        self.cell = cell = tf.compat.v1.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=True)
 
-    if input_data is None:
-      self.input_data = tf.compat.v1.placeholder(
-        tf.int32, [args.batch_size, args.seq_length])
-      self.targets = tf.compat.v1.placeholder(
-        tf.int32, [args.batch_size, args.seq_length])
-      self.initial_state = cell.zero_state(args.batch_size, tf.float32)
-    else:
-      self.input_data = input_data
-      self.targets = None
-      self.initial_state = initial_state
+        if input_data is None:
+            self.input_data = tf.compat.v1.placeholder(
+                tf.int32, [args.batch_size, args.seq_length])
+            self.targets = tf.compat.v1.placeholder(
+                tf.int32, [args.batch_size, args.seq_length])
+            self.initial_state = cell.zero_state(args.batch_size, tf.float32)
+        else:
+            self.input_data = input_data
+            self.targets = None
+            self.initial_state = initial_state
 
-    with tf.compat.v1.variable_scope('rnnlm'):
-      softmax_w = tf.compat.v1.get_variable("softmax_w",
-                                  [args.rnn_size, args.vocab_size])
-      softmax_b = tf.compat.v1.get_variable("softmax_b", [args.vocab_size])
+        with tf.compat.v1.variable_scope('rnnlm'):
+            softmax_w = tf.compat.v1.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+            softmax_b = tf.compat.v1.get_variable("softmax_b", [args.vocab_size])
 
-    embedding = tf.compat.v1.get_variable("embedding", [args.vocab_size, args.rnn_size])
-    inputs = tf.compat.v1.nn.embedding_lookup(embedding, self.input_data)
+        embedding = tf.compat.v1.get_variable("embedding", [args.vocab_size, args.rnn_size])
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
 
-    # dropout beta testing: double check which one should affect next line
-    if training and args.output_keep_prob:
-      inputs = tf.nn.dropout(inputs, args.output_keep_prob)
+        # dropout beta testing: double check which one should affect next line
+        if training and args.output_keep_prob:
+            inputs = tf.nn.dropout(inputs, args.output_keep_prob)
 
-    inputs = tf.split(inputs, args.seq_length, 1)
-    inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+        inputs = tf.split(inputs, args.seq_length, 1)
+        inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
-    def loop(prev, _):
-      prev = tf.matmul(prev, softmax_w) + softmax_b
-      prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
-      return tf.nn.embedding_lookup(embedding, prev_symbol)
+        def loop(prev, _):
+            prev = tf.matmul(prev, softmax_w) + softmax_b
+            prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
+            return tf.nn.embedding_lookup(embedding, prev_symbol)
 
-    loop_fn = loop if not training else None
-    outputs, last_state = legacy_seq2seq.rnn_decoder(inputs,
-                                                     self.initial_state,
-                                                     cell,
-                                                     loop_function=loop_fn,
-                                                     scope='rnnlm')
-    output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size])
+        loop_fn = loop if not training else None
+        outputs, last_state = legacy_seq2seq.rnn_decoder(inputs, # TODO: This method is deprecated, change to a working one
+                                                         self.initial_state,
+                                                         cell,
+                                                         loop_function=loop_fn,
+                                                         scope='rnnlm')
+        output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size])
 
-    self.final_state = last_state
+        self.final_state = last_state
+        self.logits = tf.matmul(output, softmax_w) + softmax_b
+        self.probs = tf.nn.softmax(self.logits)
 
-    self.logits = tf.matmul(output, softmax_w) + softmax_b
-    self.probs = tf.nn.softmax(self.logits)
+        # ======== Training ops etc
+        if training:
+            loss = legacy_seq2seq.sequence_loss_by_example( # TODO: This method is deprecated, change to a working one
+                [self.logits],
+                [tf.reshape(self.targets, [-1])],
+                [tf.ones([args.batch_size * args.seq_length])]
+            )
+            with tf.name_scope('cost'):
+                self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
 
-    # ======== Training ops etc
-    if training:
-      loss = legacy_seq2seq.sequence_loss_by_example(
-        [self.logits],
-        [tf.reshape(self.targets, [-1])],
-        [tf.ones([args.batch_size * args.seq_length])])
-      with tf.name_scope('cost'):
-        self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
+            self.lr = tf.Variable(0.0, trainable=False)
+            tvars = tf.trainable_variables()
+            grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), args.grad_clip)
 
-      self.lr = tf.Variable(0.0, trainable=False)
-      tvars = tf.trainable_variables()
-      grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
-                                        args.grad_clip)
-      with tf.name_scope('optimizer'):
-        optimizer = tf.train.AdamOptimizer(self.lr)
-      self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+            with tf.name_scope('optimizer'):
+                optimizer = tf.train.AdamOptimizer(self.lr)
+            self.train_op = optimizer.apply_gradients(zip(grads, tvars))
 
-      # instrument tensorboard
-      tf.summary.histogram('logits', self.logits)
-      tf.summary.histogram('loss', loss)
-      tf.summary.scalar('train_loss', self.cost)
+            # instrument tensorboard
+            tf.summary.histogram('logits', self.logits)
+            tf.summary.histogram('loss', loss)
+            tf.summary.scalar('train_loss', self.cost)
 
 
 class CharRnnLmWrapperSingleton():
-  # class singleton
-  rnn_clm = None
+    # class singleton
+    rnn_clm = None
 
-  def __init__(self, batch_size=None, sess=None, checkpoint_path=None):
-    if CharRnnLmWrapperSingleton.rnn_clm is None:
-      CharRnnLmWrapperSingleton.rnn_clm = self.load_rnn_model(batch_size,
-                                                              sess,
-                                                              checkpoint_path)
+    def __init__(self, batch_size=None, sess=None, checkpoint_path=None):
+        if CharRnnLmWrapperSingleton.rnn_clm is None:
+            CharRnnLmWrapperSingleton.rnn_clm = self.load_rnn_model(batch_size,
+                                                                    sess,
+                                                                    checkpoint_path)
 
-  def load_rnn_model(self, batch_size, sess, checkpoint_path, load_weights=True):
-    with open(os.path.join(checkpoint_path, 'config.pkl'), 'rb') as f:
-      saved_args = pickle.load(f)
-    with open(os.path.join(checkpoint_path, 'chars_vocab.pkl'), 'rb') as f:
-      chars_lm, char_inds_lm = pickle.load(f)
-    saved_args.batch_size = batch_size
+    def load_rnn_model(self, batch_size, sess, checkpoint_path, load_weights=True):
+        with open(os.path.join(checkpoint_path, 'config.pkl'), 'rb') as f:
+            saved_args = pickle.load(f)
+        with open(os.path.join(checkpoint_path, 'chars_vocab.pkl'), 'rb') as f:
+            chars_lm, char_inds_lm = pickle.load(f)
+        saved_args.batch_size = batch_size
 
-    model = CharRnnLm(saved_args, training=False)
-    if load_weights:
-      if sess is None:
-        sess = tf.compat.v1.Session()
-      with sess.as_default():
-        tf.global_variables_initializer().run()
-        saver = tf.train.Saver(tf.global_variables())
-        ckpt = tf.train.get_checkpoint_state(checkpoint_path)
-        if ckpt and ckpt.model_checkpoint_path:
-          saver.restore(sess, ckpt.model_checkpoint_path)
+        model = CharRnnLm(saved_args, training=False)
+        if load_weights:
+            if sess is None:
+                sess = tf.compat.v1.Session()
+            with sess.as_default():
+                tf.global_variables_initializer().run()
+                saver = tf.train.Saver(tf.global_variables())
+                ckpt = tf.train.get_checkpoint_state(checkpoint_path)
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess, ckpt.model_checkpoint_path)
 
-    char_inds_lm['-'] = char_inds_lm[' ']
-    del char_inds_lm[' ']
-    rnn_clm = {'model': model,
-               'chars': chars_lm,
-               'char_inds': char_inds_lm,
-               'saved_args': saved_args}
-    return rnn_clm
+        char_inds_lm['-'] = char_inds_lm[' ']
+        del char_inds_lm[' ']
+        rnn_clm = {'model': model,
+                   'chars': chars_lm,
+                   'char_inds': char_inds_lm,
+                   'saved_args': saved_args}
+
+        return rnn_clm
